03fbe497e1589373afde7fadb941f3b589cd54d8
greatpisces <2267328609@qq.com>
2017-12-28 19:17:12 +0800

    第三次上传

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeCursorWrapper.java
@@ -0,0 +1,35 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.database.Cursor;
+import android.database.CursorWrapper;
+
+import com.bignerdranch.android.criminalintent.database.CrimeDbSchema;
+
+import java.util.Date;
+import java.util.UUID;
+
+import static com.bignerdranch.android.criminalintent.database.CrimeDbSchema.CrimeTable;
+
+/**
+ * Created by Administrator on 2017/12/28.
+ */
+
+public class CrimeCursorWrapper extends CursorWrapper {
+    public CrimeCursorWrapper(Cursor cursor){
+        super(cursor);
+    }
+
+    public Crime getCrime(){
+        String uuidString = getString(getColumnIndex(CrimeTable.Cols.UUID));
+        String title = getString(getColumnIndex(CrimeTable.Cols.TITLE));
+        long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
+        int isSolved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
+
+        Crime crime = new Crime(UUID.fromString(uuidString));
+        crime.setTitle(title);
+        crime.setDate(new Date(date));
+        crime.setSolved(isSolved != 0);
+
+        return crime;
+    }
+}
